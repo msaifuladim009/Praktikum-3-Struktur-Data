1238ed532a9beb77ff4eb0b8e3e10cdac09d5d07
Muhamad Saiful Adim <56225273+msaifuladim009@users.noreply.github.com>
2020-03-03 10:23:59 +0700

    Add files via upload
    semakin terdepan

---
+++ LATIHAN 2 009.cpp
@@ -0,0 +1,51 @@
+ #include <iostream> 
+
+#include <cctype> 
+
+#include <cstring>
+
+
+
+using namespace std;
+
+
+
+int main(void)
+
+{
+
+   char teks[20], teks_baru[20];
+
+   
+
+   cout << "SAIFUL - adim | adim - SAIFUL" << endl << endl;
+
+   cout << "Tuliskan teks : "; cin >> teks;;
+
+
+
+   for (int i=0; i<strlen(teks); i++) {
+
+    if(teks[i] >= 'a' && teks[i] <= 'z'){
+
+            teks_baru[i] = toupper(teks[i]);
+
+      }
+
+      else{
+
+            teks_baru[i] = tolower(teks[i]);
+
+      }
+
+   }
+
+
+
+   cout << "Perubahan : " << teks_baru << endl;
+
+
+
+   return 0;   
+
+}
